90b7e5e0c4748fe7f4be8d17593adb225b0322a6
SB-yogeshjadhav <yogesh.jadhav@smartbear.com>
2024-10-21 15:07:38 +0530

    first commit

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0NDI1NQ/spectral.js?branch_slug=master&token=a21c8a0b-d7d2-4070-a61d-f7663046f063";
+export default { extends: ruleset };
